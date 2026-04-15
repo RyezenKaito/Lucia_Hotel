9d7b02e801cff1afc8b3ae7fb888fc8d45644f93
Daitran18031005 <tranquangdai18032005@gmail.com>
2026-04-16 02:32:42 +0700

    Cập nhật TrangThaiDatPhong.java

--- src/model/enums/TrangThaiDatPhong.java
+++ src/model/enums/TrangThaiDatPhong.java
@@ -5,7 +5,10 @@ public enum TrangThaiDatPhong {
     DA_XACNHAN("Đã xác nhận"),
     DA_CHECKIN("Đã nhận phòng"),
     DA_CHECKOUT("Đã trả phòng"),
-    DA_HUY("Đã hủy");
+    DA_HUY("Đã hủy"),
+
+    HUY_HOAN_COC("Hủy - Hoàn cọc"),
+    HUY_MAT_COC("Hủy - Mất cọc");
 
     private final String thongTinTrangThai;
 
