b6f3b6f565fa8cbd7dc041aab21855355d136130
Ryezen Kaito <khanhanpham0@gmail.com>
2026-05-24 10:42:04 +0700

    ABC

--- src/main/App.java
+++ src/main/App.java
@@ -6,7 +6,7 @@ import gui.MainFrameView;
 
 public class App {
 	// ================= MAIN =================
-
+	
 	public static void main(String[] args) {
 		javafx.application.Platform.setImplicitExit(false);
 		javafx.application.Application.launch(DangNhapView.class, args);
