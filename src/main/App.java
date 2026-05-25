a369027c52c86ff6187c258d53b086a158634ce8
Ryezen Kaito <khanhanpham0@gmail.com>
2026-05-25 09:53:10 +0700

    abc

--- src/main/App.java
+++ src/main/App.java
@@ -5,6 +5,7 @@ import gui.MainFrameView;
 
 public class App {
 	// ================= MAIN =================
+	// Khởi chạy ứng dụng
 	
 	public static void main(String[] args) {
 		javafx.application.Platform.setImplicitExit(false);
